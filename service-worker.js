647d20ae198d45516eb4e360a4c21da13d7522f6
BuildTools <dusekdan@gmail.com>
2021-08-28 11:23:44 +0100

    fix

--- service-worker.js
+++ service-worker.js
@@ -10,14 +10,14 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(cacheName).then(cache => {
             return cache.addAll(filesToCache);
-        })
+        }).catch(err => {console.log(err)})
     );
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
-            return response || fetch(e.request)
-        })
+            return response || fetch(event.request);
+        }).catch(err => {console.log(err)})
     );
 })
